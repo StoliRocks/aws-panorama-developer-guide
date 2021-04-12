a9c92407fc3e56d5a5e7a613f9fa61c3ee202d04
Michael Wunderlich <mwunderl@amazon.com>
2021-04-12 14:37:34 -0700

    use half-precision fp for faster inference

--- sample-apps/aws-panorama-sample/code/lambda_function.py
+++ sample-apps/aws-panorama-sample/code/lambda_function.py
@@ -2,9 +2,15 @@ import panoramasdk
 import cv2
 import numpy as np
 import boto3
-
+import time
+import os
+import logging
 HEIGHT = 512
 WIDTH = 512
+
+# logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class people_counter(panoramasdk.base):
 
@@ -12,7 +18,7 @@ class people_counter(panoramasdk.base):
         return {
             "parameters":
                 (
-                    ("float", "threshold", "Minimum confidence for display", 0.10),
+                    ("float", "threshold", "Minimum confidence for display", 0.50),
                     ("model", "people_counter", "Name of the model in AWS Panorama", "aws-panorama-sample-model"),
                     ("int", "batch_size", "Model batch size", 1),
                     ("float", "person_index", "The index of the person class in the model's dataset", 14),
@@ -33,27 +39,33 @@ class people_counter(panoramasdk.base):
             self.threshold = parameters.threshold
             self.person_index = parameters.person_index
             self.frame_num = 0
+            self.inference_time_ms = 0
+            self.inference_time_max = 0
+            self.frame_time_ms = 0
+            self.frame_time_max = 0
+            self.epoch_frames = 150
+            self.epoch_start = time.time()
             self.number_people = 0
             self.colours = np.random.rand(32, 3)
 
-            print("Loading model: " + parameters.people_counter)
+            logger.info("Loading model: " + parameters.people_counter)
             self.model = panoramasdk.model()
             self.model.open(parameters.people_counter, 1)
+            os.environ['TVM_TENSORRT_USE_FP16'] = '1'
 
-            print("Creating input and output arrays")
+            # create input and output arrays")
             class_info = self.model.get_output(0)
             prob_info = self.model.get_output(1)
             rect_info = self.model.get_output(2)
-
             self.class_array = np.empty(class_info.get_dims(), dtype=class_info.get_type())
             self.prob_array = np.empty(prob_info.get_dims(), dtype=prob_info.get_type())
             self.rect_array = np.empty(rect_info.get_dims(), dtype=rect_info.get_type())
 
-            print("Initialization complete")
+            logger.info("Initialization complete")
             return True
 
         except Exception as e:
-            print("Exception: {}".format(e))
+            logger.info("Exception: {}".format(e))
             return False
 
     def preprocess(self, img):
@@ -87,6 +99,7 @@ class people_counter(panoramasdk.base):
         return prob_person_indices
 
     def entry(self, inputs, outputs):
+        frame_start = time.time()
         self.frame_num += 1
 
         for i in range(len(inputs.video_in)):
@@ -98,8 +111,12 @@ class people_counter(panoramasdk.base):
             x1 = self.preprocess(person_image)
             self.model.batch(0, x1)
             self.model.flush()
+            inference_start = time.time()
             resultBatchSet = self.model.get_result()
-
+            inference_time = (time.time() - inference_start) * 1000
+            if inference_time > self.inference_time_max:
+                self.inference_time_max = inference_time
+            self.inference_time_ms += inference_time
             # Process results
             class_batch = resultBatchSet.get(0)
             prob_batch = resultBatchSet.get(1)
@@ -137,7 +154,22 @@ class people_counter(panoramasdk.base):
             
             self.model.release_result(resultBatchSet)
             outputs.video_out[i] = stream
-
+        frame_time = (time.time() - frame_start) * 1000
+        if frame_time > self.frame_time_max:
+            self.frame_time_max = frame_time
+        self.frame_time_ms += frame_time
+        if self.frame_num % self.epoch_frames == 0:
+            epoch_time = time.time() - self.epoch_start
+            logger.info('epoch length: {:.3f} s ({:.3f} FPS)'.format(epoch_time, self.epoch_frames/epoch_time))
+            logger.info('avg inference time: {:.3f} ms'.format(self.inference_time_ms / self.epoch_frames))
+            logger.info('max inference time: {:.3f} ms'.format(self.inference_time_max))
+            logger.info('avg frame processing time: {:.3f} ms'.format(self.frame_time_ms / self.epoch_frames))
+            logger.info('max frame processing time: {:.3f} ms'.format(self.frame_time_max))
+            self.inference_time_ms = 0
+            self.inference_time_max = 0
+            self.frame_time_ms = 0
+            self.frame_time_max = 0
+            self.epoch_start = time.time()
         return True
 
 
