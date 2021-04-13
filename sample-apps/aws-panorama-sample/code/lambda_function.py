b54b3261a0ac673f7e3c3a5e82ad0abaff38d8a7
Michael Wunderlich <mwunderl@amazon.com>
2021-04-12 18:19:03 -0700

    refactor

--- sample-apps/aws-panorama-sample/code/lambda_function.py
+++ sample-apps/aws-panorama-sample/code/lambda_function.py
@@ -101,59 +101,44 @@ class people_counter(panoramasdk.base):
     def entry(self, inputs, outputs):
         frame_start = time.time()
         self.frame_num += 1
-
+        # Loop through attached video streams
         for i in range(len(inputs.video_in)):
             stream = inputs.video_in[i]
-            person_image = stream.image
-            stream.add_label('People detected: {}'.format(self.number_people), 0.8, 0.05)
-
             # Prepare the image and run inference
-            x1 = self.preprocess(person_image)
-            self.model.batch(0, x1)
-            self.model.flush()
             inference_start = time.time()
+            self.model.batch(0, self.preprocess(stream.image))
+            self.model.flush()
             resultBatchSet = self.model.get_result()
             inference_time = (time.time() - inference_start) * 1000
             if inference_time > self.inference_time_max:
                 self.inference_time_max = inference_time
             self.inference_time_ms += inference_time
             # Process results
-            class_batch = resultBatchSet.get(0)
-            prob_batch = resultBatchSet.get(1)
-            rect_batch = resultBatchSet.get(2)
-
-            class_batch.get(0, self.class_array)
-            prob_batch.get(0, self.prob_array)
-            rect_batch.get(0, self.rect_array)
-
-            class_data = self.class_array[0]
-            prob_data = self.prob_array[0]
-            rect_data = self.rect_array[0]
-            
+            # Model outputs (classes, probabilities, rectangles) are collected in the BatchSet returned by model.get_result
+            # Each output is a Batch of arrays, one for each input in the batch
+            # Batch only has one image; save results for that image 
+            classes = resultBatchSet.get(0)
+            classes.get(0, self.class_array)
+            probabilities = resultBatchSet.get(1)
+            probabilities.get(0, self.prob_array)
+            boxes = resultBatchSet.get(2)
+            boxes.get(0, self.rect_array)
             # Get indices of people classes
-            person_indices = self.get_number_persons(class_data,prob_data)
-            
-            try:
-                self.number_people = len(person_indices)
-            except:
-                self.number_people = 0
-
+            person_indices = self.get_number_persons(self.class_array[0],self.prob_array[0])
+            self.number_people = len(person_indices)
             # Draw bounding boxes on output image
-            if self.number_people > 0:
-                for index in person_indices:
-    
-                    left = np.clip(rect_data[index][0] / np.float(HEIGHT), 0, 1)
-                    top = np.clip(rect_data[index][1] / np.float(WIDTH), 0, 1)
-                    right = np.clip(rect_data[index][2] / np.float(HEIGHT), 0, 1)
-                    bottom = np.clip(rect_data[index][3] / np.float(WIDTH), 0, 1)
-    
-                    stream.add_rect(left, top, right, bottom)
-                    stream.add_label(str(prob_data[index][0]), right, bottom)
+            for index in person_indices:
+                left = np.clip(self.rect_array[0][index][0] / np.float(HEIGHT), 0, 1)
+                top = np.clip(self.rect_array[0][index][1] / np.float(WIDTH), 0, 1)
+                right = np.clip(self.rect_array[0][index][2] / np.float(HEIGHT), 0, 1)
+                bottom = np.clip(self.rect_array[0][index][3] / np.float(WIDTH), 0, 1)
+                stream.add_rect(left, top, right, bottom)
+                stream.add_label(str(self.prob_array[0][index][0]), right, bottom)
             # Add text
-            stream.add_label('People detected: {}'.format(self.number_people), 0.8, 0.05)
-            
+            stream.add_label('People detected: {}'.format(self.number_people), 0, 0.9)
             self.model.release_result(resultBatchSet)
             outputs.video_out[i] = stream
+        # Log metrics
         frame_time = (time.time() - frame_start) * 1000
         if frame_time > self.frame_time_max:
             self.frame_time_max = frame_time
